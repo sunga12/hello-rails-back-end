80e4993b87d0f328280bef745be2af6494a1b422
Sunga Thawethe <sungabt@gmail.com>
2023-11-07 19:44:30 +0200

    Update files

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   def random_greetings
     @random_greeting = Greeting.order('RANDOM()').first.message
+    
     render json: @random_greeting
   end
 end
