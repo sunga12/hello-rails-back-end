1e34d1a36392a839971d6a1f94aac8d352097f31
Sunga Thawethe <sungabt@gmail.com>
2023-11-07 19:19:00 +0200

    Setup branch

--- app/controllers/api/v1/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::GreetingsController < ApplicationController
-  def random_greetings
-    @random_greeting = Greeting.order('RANDOM()').first.message
-    render json: @random_greeting
-  end
-end
